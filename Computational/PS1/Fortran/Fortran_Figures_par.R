b2d2fbe18c389e0931a2ef80ba289b146d1c6699
John Higgins <johnnyhiggins0@gmail.com>
2022-09-11 09:58:21 -0500

    Remembered to save R file

---
+++ Computational/PS1/Fortran/Fortran_Figures_par.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+valuefun$pf_h <- polfun$X2
+valuefun$pf_l <- polfun$X3
+
+colnames(valuefun) <- c("Capital", "high_VF", "low_VF", "high_PF", "low_PF")
+ggplot(valuefun) +   geom_line(aes(x=Capital,y=high_VF, group=1, colour = 'High productivity')) +    geom_line(aes(x=Capital,y=low_VF, group = 2, colour = 'Low productivity')) +   ylab('Value')+xlab('Capital') + ggtitle("Value functions by productivity type (Fortran-parallelized)")
+ggplot(valuefun) +   geom_line(aes(x=Capital,y=high_PF, group=1, colour = 'High productivity')) +    geom_line(aes(x=Capital,y=low_PF, group = 2, colour = 'Low productivity')) +   ylab('Policy')+xlab('Capital') + ggtitle("Policy functions by productivity type (Fortran-parallelized)")
