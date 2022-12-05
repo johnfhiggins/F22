11a46070a64d093f63f4fe653dc54f99771d5496
John Higgins <johnnyhiggins0@gmail.com>
2022-12-05 11:23:28 -0600

    Fixed my dumb mistakes

--- IO/PS6/PS6.R
+++ IO/PS6/PS6.R
@@ -8,7 +8,7 @@ fpa$M4 <- apply(cbind(fpa$V1, fpa$V2, fpa$V3),1,max,na.rm=TRUE)
 fpa$max <- apply(cbind(fpa$V1, fpa$V2, fpa$V3, fpa$V4),1,max,na.rm=TRUE)
 
 h <- 20
-grid <- seq(0, 160, 0.1)
+grid <- seq(0, 200, 0.1)
 kd_est <- data.frame(grid)
 kd_est$G <- 0
 kd_est$g <- 0
@@ -37,7 +37,7 @@ markdown3 <- approxfun(grid, y=kd_est$markdown3)
 markdown4 <- approxfun(grid, y=kd_est$markdown4)
 
 fpa$u1 <- fpa$V1 + markdown1(fpa$V1)
-fpa$u2 <- fpa$V2 + markdown2$(fpa$V2)
+fpa$u2 <- fpa$V2 + markdown2(fpa$V2)
 fpa$u3 <- fpa$V3 + markdown3(fpa$V3)
 fpa$u4 <- fpa$V4 + markdown4(fpa$V4)
 
@@ -51,7 +51,7 @@ plot(fpa$u4, fpa$V4, xlim=c(0, max(fpa$u4)), ylim=c(0, max(fpa$u4)), main="Bidde
 abline(0,1)
 
 #construct f_U
-u_grid <- seq(0, 325, 0.2)
+u_grid <- seq(0, 350, 0.2)
 
 u1_L <- quantile(fpa$u1, probs=0.25)
 u1_H <- quantile(fpa$u1, probs=0.75)
@@ -138,38 +138,39 @@ lines(marg_dist$u, marg_dist$dens4, col="green")
 legend(200, 0.005,legend=c("Marginal distribution \n (under ind + sym) ", "Bidder 1 Marginal", "Bidder 2 Marginal", "Bidder 3 Marginal", "Bidder 4 Marginal"), col=c("black", "red", "orange", "blue", "green"), lty=1, lwd=c(3, 1,1,1,1))
 
 
-f_dist <- approxfun(f_indsym$x, f_indsym$y, yleft=0, yright=0)
-f_cdf <- apply(cbind(-Inf, f_indsym$x), 1, function(x) {integrate(f_dist, lower=x[1], upper=x[2])$value}) #ecdf(unlist(stack(fpa, select=c("u1", "u2", "u3", "u4"))[1]))
+f_dist <- approxfun(f_indsym$x, f_indsym$y, yleft=0, yright=1)
+#f_cdf <- apply(cbind(-Inf, f_indsym$x), 1, function(x) {integrate(f_dist, lower=x[1], upper=x[2])$value}) 
+f_cdf <- ecdf(unlist(stack(fpa, select=c("u1", "u2", "u3", "u4"))[1]))
+
 
 f_1 <- approxfun(est1$x, est1$y, yleft=0, yright=0)
-cdf_1 <- apply(cbind(0, est1$x), 1, function(x) {integrate(f_1, lower=x[1], upper=x[2])$value})
+#cdf_1 <- apply(cbind(0, est1$x), 1, function(x) {integrate(f_1, lower=x[1], upper=x[2])$value})
 f_2 <- approxfun(est2$x, est2$y, yleft=0, yright=0)
-cdf_2 <- apply(cbind(0, est2$x), 1, function(x) {integrate(f_2, lower=x[1], upper=x[2])$value})
+#cdf_2 <- apply(cbind(0, est2$x), 1, function(x) {integrate(f_2, lower=x[1], upper=x[2])$value})
 f_3 <- approxfun(est3$x, est3$y, yleft=0, yright=0)
-cdf_3 <- apply(cbind(0, est3$x), 1, function(x) {integrate(f_3, lower=x[1], upper=x[2])$value})
+#cdf_3 <- apply(cbind(0, est3$x), 1, function(x) {integrate(f_3, lower=x[1], upper=x[2])$value})
 f_4 <- approxfun(est4$x, est4$y, yleft=0, yright=0)
-cdf_4 <- apply(cbind(0, est4$x), 1, function(x) {integrate(f_4, lower=x[1], upper=x[2])$value})
+#cdf_4 <- apply(cbind(0, est4$x), 1, function(x) {integrate(f_4, lower=x[1], upper=x[2])$value})
+cdf_1 <- ecdf(fpa$u1)
+cdf_2 <- ecdf(fpa$u2)
+cdf_3 <- ecdf(fpa$u3)
+cdf_4 <- ecdf(fpa$u4)
 
-plot(f_indsym$x,f_cdf, type='l',lwd=5,  main ="CDF of valuations assuming independence and symmetry \n (plotted with individual marginals for comparison)")
-lines(est1$x, cdf_1, col="orange")
-lines(est2$x, cdf_2, col="red")
-lines(est3$x, cdf_3, col="blue")
-lines(est4$x, cdf_4, col="green")
+plot(f_cdf,lwd=5,  main ="Empirical CDF of valuations assuming independence and symmetry \n (plotted with individual marginals for comparison)")
+lines(cdf_1, type='l', cex=0.2, col="orange")
+lines(cdf_2, col="red", cex=0.2)
+lines(cdf_3, col="blue", cex=0.2)
+lines( cdf_4, col="green", cex=0.2)
 legend(210, 0.4,legend=c("CDF of valuations \n (under ind + sym) ", "Bidder 1", "Bidder 2", "Bidder 3", "Bidder 4 "), col=c("black", "orange", "red", "blue", "green"), lty=1, lwd=c(5, 1,1,1,1))
 
 
 
 #symmetric independent private values
 h_g <- 20
-#second_highest <- function(x) order(x, decreasing=TRUE)[2]
-#fpa$second <- apply(cbind(fpa$V1, fpa$V2, fpa$V3, fpa$V4),1, function(x) x[second_highest(x)])
-#g <- density(fpa$second, from=0, to=max(u_grid))
-#g_int <- approxfun(g$x, g$y, yleft=0, yright=0)
-#G_vals <- apply(cbind(-Inf, g$x), 1, function(x) {integrate(g_int, lower=x[1], upper=x[2])$value})
-#G <- approxfun(g$x, G_vals, yleft=0, yright=1)
 
-g <- f_indsym
-G_vals <- f_cdf
+g <- density(unlist(stack(fpa, select=c("V1", "V2", "V3", "V4"))[1]), from=0, to=max(grid))
+g_dist <- approxfun(g$x, g$y, yleft=0, yright=0)
+G_vals <- apply(cbind(-Inf, g$x), 1, function(x) {integrate(g_dist, lower=x[1], upper=x[2])$value})
 G <- approxfun(g$x, G_vals, yleft=0, yright=1)
 
 fpa$uhat_1 <- fpa$V1 + G(fpa$V1)/(3*g_int(fpa$V1))
@@ -182,17 +183,18 @@ f2 <- density(fpa$uhat_2, from=0, to=max(u_grid))
 f3 <- density(fpa$uhat_3, from=0, to=max(u_grid))
 f4 <- density(fpa$uhat_4, from=0, to=max(u_grid))
 
-f_SIPV <- density(unlist(stack(fpa, select=c("uhat_1", "uhat_2", "uhat_3", "uhat_4"))[1]), from=0, to=max(u_grid))
+f_SIPV <- density(unlist(stack(fpa, select=c("uhat_1", "uhat_2", "uhat_3", "uhat_4"))[1]), from=0, to=max(350))
 f_SIPV_int <- approxfun(f_SIPV$x, f_SIPV$y, yleft=0, yright=0)
-F_SIPV <- apply(cbind(-Inf, u_grid), 1, function(x) {integrate(f_SIPV_int, lower=x[1], upper=x[2])$value})
-F_SIPV_fun <- approxfun(u_grid, F_SIPV, yleft=0, yright=1)
+F_SIPV <- ecdf(unlist(stack(fpa, select=c("uhat_1", "uhat_2", "uhat_3", "uhat_4"))[1]))
+#F_SIPV <- apply(cbind(-Inf, u_grid), 1, function(x) {integrate(f_SIPV_int, lower=x[1], upper=x[2])$value})
+#F_SIPV_fun <- approxfun(u_grid, F_SIPV, yleft=0, yright=1)
 
 
-plot(f_SIPV, lwd=3, main ="Marginal distribution of valuations under initial SIPV assumption \n (plotted with previous marginal for comparison)")
+plot(f_SIPV, lwd=3, main ="Estimated marginal distribution of valuations under initial SIPV assumption \n (plotted with previous marginal for comparison)")
 lines(f_indsym$x,f_indsym$y, type="l", col="red", lwd=3)
 legend(150, 0.008, legend=c("Marginal distribution (under init. SIPV assn.) ", "Previous marginal (not under initial SIPV)"), col=c("black", "red" ), lty=1, lwd=c(3, 3))
 
-plot(F_SIPV_fun, xlim=c(0,300), type='l', col="black", lwd=3, main="CDF of valuations under initial SIPV assumption \n (plotted with previous CDF for comparison")
-lines(f_indsym$x, f_cdf, lwd=3, col="red")
+plot(F_SIPV, xlim=c(0,max(u_grid)),col="black", lwd=3, main="Empirical CDF of valuations under initial SIPV assumption \n (plotted with previous CDF for comparison)")
+lines( f_cdf, lwd=3, col="red")
 legend(120, 0.15, legend=c("CDF of valuations (under init. SIPV assn.) ", "Previous CDF (not under initial SIPV)"), col=c("black", "red" ), lty=1, lwd=c(3, 3))
 
